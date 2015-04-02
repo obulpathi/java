8693bea8b2554e0795efa1dd19609bf854c16a1b
Obulpathi <obulpathi@gmail.com>
2015-04-02 08:13:47 -0400

    Syntax

--- 02-Syntax/Syntax.java
+++ 02-Syntax/Syntax.java
@@ -15,8 +15,23 @@ public class Syntax {
 
         if(i < j) {
             System.out.println("First < Second");
+        } else if (i > j) {
+            System.out.println("First > Second");
+        } else {
+            System.out.println("First = Second");
         }
-        
+
+        for(i = 0; i < 10; i++) {
+            System.out.print(i);
+        }
+        System.out.println("");
+
+        while(i > 0){
+            System.out.print(i-1);
+            i--;
+        }
+        System.out.println("");
+
         System.out.println("");
     }
 }
