3ba5e8848478bc3c339587637c0429d365e711a7
Obulpathi <obulpathi@gmail.com>
2015-04-03 17:53:26 -0400

    Enhanced for loop

--- 02-Syntax/Syntax.java
+++ 02-Syntax/Syntax.java
@@ -61,5 +61,11 @@ public class Syntax {
 
         Day day = Day.Monday;
         System.out.println(day);
+
+        int[] array = {1,2,3,4,5,6,7,8,9,0};
+        for (int a: array) {
+            System.out.printf("%d ", a);
+        }
+        System.out.println("");
     }
 }
